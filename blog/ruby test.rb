8418f0ed4f78b7cb03f17b3deb7fb45afae33cb1
kaylindenberg <kay.lindneberg@gmail.com>
2014-12-13 00:03:38 -0600

    removed ruby test file that I hadn't meant to upload

--- blog/ruby test.rb
+++
@@ -1,7 +0,0 @@
-chicago_crabs=["Chase Nutile", "David Vaynshteyn", "Deepankara Reddy", "Jordan Obey", "Kay Lindenberg", "Nadia Bajwa", "Ricardo Fleury", "Sara Chen", "Shannon Staszak", "Stephanie Marx", "Susan Schmidt", "Tyler Kowalewski", "Tyler Pate"]
-
-print chicago_crabs.group_by {|name| 3}
-
-puts (1..15).group_by
-
-print (1..15).group_by {|i| i%3} 
